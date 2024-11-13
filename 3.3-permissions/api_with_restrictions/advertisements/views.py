7d090974ffee0520e867709c0c6d3abed83a9fd5
Oleg Avdeev <aotransportcompany@gmail.com>
2024-11-13 11:21:07 +0300

    Доработка решения к заданию 3.3

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,9 +1,19 @@
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from rest_framework.exceptions import PermissionDenied
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from advertisements.models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
 from advertisements.filters import AdvertisementFilter
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return obj.creator == request.user
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -12,10 +22,24 @@ class AdvertisementViewSet(ModelViewSet):
     serializer_class = AdvertisementSerializer
     filterset_class = AdvertisementFilter
     throttle_classes = [UserRateThrottle, AnonRateThrottle]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['created_at', 'updated_at']
+    search_fields = ['title', 'description']
 
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update", "destroy"]:
-            return [IsAuthenticated()]
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
         return []
+
+    def perform_destroy(self, instance):
+        if instance.creator != self.request.user:
+            raise PermissionDenied("Вы не можете удалить это объявление.")
+        instance.delete()
+
+    def perform_update(self, serializer):
+        if self.get_object().creator != self.request.user:
+            raise PermissionDenied("Вы не можете обновить это объявление.")
+        serializer.save()
+
 
